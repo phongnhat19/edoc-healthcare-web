9594f6418227c2ab75c91abd3d618b2b311374ab
Nhat Nguyen <phongnhat19@gmail.com>
2020-06-29 21:20:38 +0700

    add form detail page

---
+++ src/page/Forms/FormDetail/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormDetail = () => {
+  return (
+    <div className="app-inner-content-layout">
+      <div className="app-inner-content-layout--main">Form detail</div>
+    </div>
+  );
+};
+
+export default FormDetail;
